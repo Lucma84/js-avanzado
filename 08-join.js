fd911fe121698b8a4dc56492367dbf11cbe94af7
Lucma84 <carlosromo1@gmail.com>
2023-01-22 11:37:29 +0100

    08-join

---
+++ 08-join.js
@@ -0,0 +1,14 @@
+// Dado un array, nos permite concatenar los strings del array con espacios.
+
+const words = ['Hola', 'que', 'tal', '?'];
+
+const sentence = words.join(' '); // \n es un salto de linea en la terminal
+
+// let sentence = '';
+
+// for (let i = 0; i < words.length; i++) {
+//   sentence += words[i];
+//   sentence += ' ';
+// }
+
+console.log(sentence);
